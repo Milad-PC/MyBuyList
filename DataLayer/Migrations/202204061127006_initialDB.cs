6822104ffcd3e966e59b5504138402296f69d2cc
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2022-04-06 16:00:33 +0430

    update database

---
+++ DataLayer/Migrations/202204061127006_initialDB.cs
@@ -0,0 +1,84 @@
+﻿namespace DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Lists",
+                c => new
+                    {
+                        ListID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Date = c.DateTime(nullable: false),
+                        MakerID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ListID);
+            
+            CreateTable(
+                "dbo.Partners",
+                c => new
+                    {
+                        PartnerID = c.Int(nullable: false, identity: true),
+                        UserID = c.Int(nullable: false),
+                        ListID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PartnerID)
+                .ForeignKey("dbo.Lists", t => t.ListID, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserID, cascadeDelete: true)
+                .Index(t => t.UserID)
+                .Index(t => t.ListID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        UserName = c.String(nullable: false, maxLength: 50),
+                        Password = c.String(nullable: false),
+                        Phone = c.String(nullable: false, maxLength: 11),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        TaskID = c.Int(nullable: false, identity: true),
+                        ListID = c.Int(nullable: false),
+                        Title = c.String(nullable: false, maxLength: 200),
+                        Date = c.DateTime(nullable: false),
+                        DoneDate = c.DateTime(),
+                        IsDone = c.Boolean(nullable: false),
+                        UserID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TaskID)
+                .ForeignKey("dbo.Lists", t => t.ListID, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserID, cascadeDelete: true)
+                .Index(t => t.ListID)
+                .Index(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tasks", "UserID", "dbo.Users");
+            DropForeignKey("dbo.Tasks", "ListID", "dbo.Lists");
+            DropForeignKey("dbo.Partners", "UserID", "dbo.Users");
+            DropForeignKey("dbo.Partners", "ListID", "dbo.Lists");
+            DropIndex("dbo.Tasks", new[] { "UserID" });
+            DropIndex("dbo.Tasks", new[] { "ListID" });
+            DropIndex("dbo.Partners", new[] { "ListID" });
+            DropIndex("dbo.Partners", new[] { "UserID" });
+            DropTable("dbo.Tasks");
+            DropTable("dbo.Users");
+            DropTable("dbo.Partners");
+            DropTable("dbo.Lists");
+        }
+    }
+}
---
+++ DataLayer/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+﻿namespace DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataLayer.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DataLayer.MyContext context)
+        {
+            if (!context.Users.Any())
+            {
+                User usr = new User()
+                {
+                    UserName = "Milad",
+                    Password = "123456789",
+                    Date = DateTime.Now,
+                    Name = "میلاد اسدی",
+                    Phone = "09117449564"
+                };
+                context.Users.Add(usr);
+                context.SaveChanges();
+            }
+        }
+    }
+}

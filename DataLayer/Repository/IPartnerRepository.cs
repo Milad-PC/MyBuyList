7620a9f8c9f8d7d9d7ce1e079a51b0518c6e1cdc
Rasoul Fallah <m.rasoul.fallah@gmail.com>
2022-04-06 13:58:06 +0430

    Update IPartnerRepository.cs

--- DataLayer/Repository/IPartnerRepository.cs
+++ DataLayer/Repository/IPartnerRepository.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer.Repository
+namespace DataLayer
 {
-    internal interface IPartnerRepository
+    public interface IPartnerRepository:IDisposable
     {
+        IEnumerable<Partner> GetAllPartners();
+        IEnumerable<Partner> GetPartnersByList();
+
+        Partner GetPartnerByID(int partnerId); 
+
+        bool Insert(Partner partner);
+        bool Update(Partner partner);
+        bool Delete(Partner partner);
+        bool Delete(int partnerId);
+
+        void Save();
+
+
+
     }
 }

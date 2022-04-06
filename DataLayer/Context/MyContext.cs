3d94f3fe0397f9a798a4a521465053ec32c6d2ad
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2022-04-06 13:46:09 +0430

    Update Context

--- DataLayer/Context/MyContext.cs
+++ DataLayer/Context/MyContext.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer.Context
 {
-    internal class MyContext
+    public class MyContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<List>  Lists { get; set; }
+        public DbSet<Partner> Partners { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
     }
 }
